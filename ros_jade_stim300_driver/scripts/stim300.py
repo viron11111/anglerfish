13d641e04b8ccda124b6b355a162618f745529ed
Andy <andygrayia@gmail.com>
2016-05-30 18:37:39 -0400

    ANGLERFISH: first commit of STIM300 driver

---
+++ ros_jade_stim300_driver/scripts/stim300.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+
+import serial
+import numpy as np
+import rospy
+from geometry_msgs.msg import Quaternion, Vector3
+from sensor_msgs.msg import Imu
+from std_msgs.msg import Header
+import binascii
+import struct
+import encodings
+#import sub8_ros_tools
+
+
+class Interface(object):
+    _baudrate = 921600 #(1000 bits per second)
+    _datagram_lengths = {
+        chr(0x93): 40 - 2,  #-2 Rate, acceleration, inclination
+    }
+    # Rate, acceleration, incliination
+
+    def __init__(self):
+        self.serial = serial.Serial('/dev/ttyUSB0', baudrate=self._baudrate)
+        self.datagram_identifier = chr(0x93) #Rate, acceleration, and inclination
+        self.imu_pub = rospy.Publisher('/imu', Imu, queue_size=1)
+        #self.last_msg = Nonesens
+        self.skipped_msgs = 0
+
+    def sync(self):
+        char = None
+        k = 0
+        self.msg = ""
+        check = None
+        while(1):
+            check = self.serial.read(1)
+            check = check.encode("hex")
+            if  check == "93":
+                break
+        while k < 37:#self.datagram_identifier:
+            char = self.serial.read(1)
+            self.msg = self.msg + char
+            k += 1
+            #char = self.serial.read(1)
+        #print self.msg.encode("hex")
+        #print self.msg[0:9].encode("hex"), "", self.msg[10:19].encode("hex"), "", self.msg[20:29].encode("hex")
+
+        return k
+
+    def read_datagram(self):
+        msg = self.serial.read(
+            self._datagram_lengths[self.datagram_identifier]
+        )
+ 
+
+        start = 0
+        
+        # gyr
+        gyro = np.fromstring(
+            b'\x00' + self.msg[start + 0:start + 3][::-1] +
+            b'\x00' + self.msg[start + 3:start + 6][::-1] +
+            b'\x00' + self.msg[start + 6:start + 9][::-1],
+            dtype='<i'
+        ).astype(np.float32) / (2 ** 14)
+        print gyro
+
+        self.last_msg = gyro
+
+        start += 10
+        # acc
+        linear_acceleration = np.fromstring(
+            b'\x00' + self.msg[start + 0:start + 3][::-1] +
+            b'\x00' + self.msg[start + 3:start + 6][::-1] +
+            b'\x00' + self.msg[start + 6:start + 9][::-1],
+            dtype='<i'
+        ).astype(np.float32) / (2 ** 19)
+        start += 10
+
+        # inc
+        inclination = np.fromstring(
+            b'\x00' + msg[start + 0:start + 3][::-1] +
+            b'\x00' + msg[start + 3:start + 6][::-1] +
+            b'\x00' + msg[start + 6:start + 9][::-1],
+            dtype='<i'
+        ).astype(np.float32) / (2 ** 22)
+
+#header=sub8_ros_tools.make_header(),
+
+        imu_msg = Imu(            
+            header= Header(
+                stamp=rospy.get_rostime()
+            ),
+            angular_velocity=Vector3(*gyro),
+            linear_acceleration=Vector3(*linear_acceleration)
+            #orientation=Quaternion(inclination)
+        )
+        self.imu_pub.publish(imu_msg)
+
+        #print msg.encode("hex")
+
+    def run(self):
+        l = self.sync()
+        l = 38
+        if l != 38:
+            return
+        self.read_datagram()
+
+
+if __name__ == '__main__':
+    rospy.init_node('stim300_gyro')
+    i = Interface()
+    while(True):
+        i.run()
