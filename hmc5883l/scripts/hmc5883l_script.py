adff32de7cdb8e9114993f81ca6e5b400e14bc86
andy <andygrayia@gmail.com>
2016-06-06 12:37:07 -0400

    ANGLERFISH: shift script to Anglerfish

--- hmc5883l/scripts/hmc5883l_script.py
+++ hmc5883l/scripts/hmc5883l_script.py
@@ -5,5 +5,36 @@ import smbus
 import time
 import math
 import rospy
-from std_msgs.msg import Float32
+from std_msgs.msg import Float64
 from std_msgs.msg import Header
+from geometry_msgs.msg import Vector3
+from sensor_msgs.msg import MagneticField
+
+
+
+class measure_headings():
+
+	def __init__(self):
+		self.bus = smbus.SMBus(1)
+
+		self.HMC5883L_ADDR       = 0x1E  #HMC5883L address
+
+		self.mag_pub = rospy.Publisher("magnetic_headings", MagneticField, queue_size=1)
+
+
+
+def main(args):
+	rospy.init_node('hmc5883l_magnetometer', anonymous=False)
+
+	measure_headings()
+
+        try:
+		rospy.spin()
+        except rospy.ROSInterruptException:
+		print "Shutting down"
+                pass
+	
+
+
+if __name__ == '__main__':
+	main(sys.argv)
