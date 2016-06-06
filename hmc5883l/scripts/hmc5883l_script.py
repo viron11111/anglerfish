e952033fdccd969b5611d3e970c128969a9fdbfd
Andy <andygrayia@gmail.com>
2016-06-06 18:09:48 +0000

    ANGLERFISH: transfer to sublime

--- hmc5883l/scripts/hmc5883l_script.py
+++ hmc5883l/scripts/hmc5883l_script.py
@@ -10,62 +10,53 @@ from std_msgs.msg import Header
 from geometry_msgs.msg import Vector3
 from sensor_msgs.msg import MagneticField
 
-def read_byte(adr):
-    return bus.read_byte_data(address, adr)
-
-def read_word(adr):
-    high = bus.read_byte_data(address, adr)
-    low = bus.read_byte_data(address, adr+1)
-    val = (high << 8) + low
-    return val
-
-def read_word_2c(adr):
-    val = read_word(adr)
-    if (val >= 0x8000):
-        return -((65535 - val) + 1)
-    else:
-        return val
-
-def write_byte(adr, value):
-    bus.write_byte_data(address, adr, value)
-
-
-
 class measure_headings():
 
-	def read_byte(adr):
-    	return bus.read_byte_data(address, adr)
+	def read_byte(self, adr):
+	    return self.bus.read_byte_data(self.HMC5883L_ADDR, adr)
 
-	def read_word(adr):
-	    high = bus.read_byte_data(address, adr)
-	    low = bus.read_byte_data(address, adr+1)
+	def read_word(self, adr):
+	    high = self.bus.read_byte_data(self.HMC5883L_ADDR, adr)
+	    low = self.bus.read_byte_data(self.HMC5883L_ADDR, adr+1)
 	    val = (high << 8) + low
 	    return val
 	    
-	def read_word_2c(adr):
-	    val = read_word(adr)
+	def read_word_2c(self, adr):
+	    val = self.read_word(adr)
 	    if (val >= 0x8000):
 	        return -((65535 - val) + 1)
 	    else:
 	        return val
 
-	def write_byte(adr, value):
-	    bus.write_byte_data(address, adr, value)
+	def write_byte(self, adr, value):
+	    self.bus.write_byte_data(self.HMC5883L_ADDR, adr, value)
+
+        def get_reading(self): 
+            self.x_out = self.read_word_2c(3) * self.scale
+            self.y_out = self.read_word_2c(7) * self.scale
+            self.z_out = self.read_word_2c(5) * self.scale
+            #print x_out
 
 	def __init__(self):
-		self.bus = smbus.SMBus(1)
+	    self.bus = smbus.SMBus(1)
 
-		self.HMC5883L_ADDR       = 0x1E  #HMC5883L address
+	    self.HMC5883L_ADDR       = 0x1E  #HMC5883L address
 
-		self.mag_pub = rospy.Publisher("magnetic_headings", MagneticField, queue_size=1)
+	    self.mag_pub = rospy.Publisher("magnetic_headings", MagneticField, queue_size=1)
 
-		self.write_byte(0x00, 0b01110000) # Set to 8 samples @ 15Hz
-		self.write_byte(0x01, 0b00100000) # 1.3 gain LSb / Gauss 1090 (default)
-		self.write_byte(0x02, 0b00000000) # Continuous sampling
+	    self.write_byte(0x00, 0b01110000) # Set to 8 samples @ 15Hz
+	    self.write_byte(0x01, 0b00100000) # 1.3 gain LSb / Gauss 1090 (default)
+	    self.write_byte(0x02, 0b00000000) # Continuous sampling
 
-	def get_reading(self):
+            self.scale = 0.92
 
+            rate = rospy.Rate(15) #for 15 Hz readings
 
+            while not rospy.is_shutdown():
+                self.get_reading()
+		
+                print self.x_out
+                rate.sleep()
 
 def main(args):
 	rospy.init_node('hmc5883l_magnetometer', anonymous=False)
