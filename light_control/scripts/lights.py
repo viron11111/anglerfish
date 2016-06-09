ab982c34b71257d4d00118a8d67d182b75a7bcd6
Andy <andygrayia@gmail.com>
2016-06-09 20:49:18 +0000

    ANGLERFISH: lights with pigpiod

--- light_control/scripts/lights.py
+++ light_control/scripts/lights.py
@@ -9,20 +9,22 @@ from std_msgs.msg import Bool, String
 class start_lights():
 
 	def green(self, power):
-		if power.data == 1:
-			self.pi.write(16, 1)
-		else:
-			self.pi.write(16, 0)
+	    if power.data == 1:
+		self.pig.write(16, 1)
+	    else:
+		self.pig.write(16, 0)
 
 	def white(self, power):
 	    if power.data == 1:
-	        self.pi.write(12, 1)
+	        self.pig.write(12, 1)
 	    else:
-	        self.pi.write(12, 0)
+	        self.pig.write(12, 0)
 
-	def lights_off(self):
-		self.pi.write(12, 0)
-		self.pi.write(16, 0)		
+	def lights_off():
+		pi1 = pigpio.pi()
+		pi1.write(12, 0)
+		pi1.write(16, 0)		
+		pi1.stop()
 
 	def __init__(self):
 		rospy.Subscriber('Green_led', Bool, self.green)
@@ -30,9 +32,9 @@ class start_lights():
 		self.green_pub = rospy.Publisher("green_light", String, queue_size=1)
 		self.white_pub = rospy.Publisher("white_light", String, queue_size=1)
 
-		self.pi = pigpio.pi()
-		self.pi.set_mode(12, pigpio.OUTPUT)
-		self.pi.set_mode(16, pigpio.OUTPUT)
+		self.pig = pigpio.pi()
+		self.pig.set_mode(12, pigpio.OUTPUT)
+		self.pig.set_mode(16, pigpio.OUTPUT)
 
 		rate = rospy.Rate(10) #Hz
 
@@ -40,11 +42,7 @@ class start_lights():
 
 		    rate.sleep()	
 
-    rospy.on_shutdown(self.lights_off)
-
-
-
-rospy.on_shutdown(lights_off)
+    	rospy.on_shutdown(lights_off)
 
 if __name__ == '__main__':
         rospy.init_node('LED_control', anonymous=False)
