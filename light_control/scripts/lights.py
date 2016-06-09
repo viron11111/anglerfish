031550e68ffbc6714a13c932dc821eb937ea5193
andy <andygrayia@gmail.com>
2016-06-09 15:45:52 -0400

    ANGLERFISH: use pigpio library with light script

--- light_control/scripts/lights.py
+++ light_control/scripts/lights.py
@@ -1,39 +1,49 @@
 #!/usr/bin/python
 
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
+import pigpio
 import time
 import rospy
-from std_msgs.msg import Bool  
+from std_msgs.msg import Bool, String
+
+class start_lights():
+
+	def green(self, power):
+		if power.data == 1:
+			self.pi.write(16, 1)
+		else:
+			self.pi.write(16, 0)
+
+	def white(self, power):
+	    if power.data == 1:
+	        self.pi.write(12, 1)
+	    else:
+	        self.pi.write(12, 0)
+
+	def lights_off(self):
+		self.pi.write(12, 0)
+		self.pi.write(16, 0)		
+
+	def __init__(self):
+		rospy.Subscriber('Green_led', Bool, self.green)
+		rospy.Subscriber('White_led', Bool, self.white)
+		self.green_pub = rospy.Publisher("green_light", String, queue_size=1)
+		self.white_pub = rospy.Publisher("white_light", String, queue_size=1)
+
+		self.pi = pigpio.pi()
+		self.pi.set_mode(12, pigpio.OUTPUT)
+		self.pi.set_mode(16, pigpio.OUTPUT)
+
+		rate = rospy.Rate(10) #Hz
+
+		while not rospy.is_shutdown():
+
+		    rate.sleep()	
+
+    rospy.on_shutdown(self.lights_off)
 
 
-def green(power):
-	if power.data == 1:
-		GPIO.output(16, 1)
-	else:
-		GPIO.output(16, 0)
 
-def white(power):
-        if power.data == 1:
-	        GPIO.output(12, 1)
-        else:
-                GPIO.output(12, 0)
-
-def lights_off():
-	GPIO.output(12, 0)
-	GPIO.output(16, 0)
-	GPIO.cleanup()		
-
-def start_lights():
-	rospy.Subscriber('Green_led', Bool, green)
-	rospy.Subscriber('White_led', Bool, white)
-
-	GPIO.setmode(GPIO.BOARD)
-	#GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-
-	GPIO.setup(12, GPIO.OUT) #white light
-	GPIO.setup(16, GPIO.OUT) #green light
-		
 rospy.on_shutdown(lights_off)
 
 if __name__ == '__main__':
