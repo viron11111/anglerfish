4980d649d281d4ddfb96bb2189a240830917ffcd
andy <andygrayia@gmail.com>
2016-06-10 16:07:12 -0400

    ANGLERFISH: push before pool day

---
+++ camera_stabilization/scripts/stabilizer.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import rospy
+from std_msgs.msg import Header
+from std_msgs.msg import Float32, Int16
+from orientation_estimater.msg import rpy_msg
+import math
+
+class stability(object):
+
+	def __init__(self):
+
+		rospy.Subscriber("rpy_msg", rpy_msg, self.stabilize)
+		self.servo_pub = rospy.Publisher("servo_position", Int16, queue_size=1)
+
+		self.servocmd = 400
+
+	def stabilize(self, data):
+
+		output = int(-data.pitch*215 + 400)
+		if output <= 140:
+			output = 140
+		elif output >= 710:
+			ouput = 710
+		#print output
+		self.servo_pub.publish(output)
+
+def main():
+	rospy.init_node('camera_stabilization_node', anonymous=False)
+
+	stability()
+
+	try:
+		rospy.spin()
+	except rospy.ROSInterruptException:
+		print "Shutting down"
+		pass
+
+
+if __name__ == '__main__':
+	main() #sys.argv
--- orientation_estimater/scripts/estimater.py
+++ orientation_estimater/scripts/estimater.py
@@ -7,6 +7,9 @@ import geometry_msgs.msg
 from sensor_msgs.msg import Imu
 from std_msgs.msg import Header
 from std_msgs.msg import Float32
+from anglerfish.msg import ms5837 
+from orientation_estimater.msg import rpy_msg
+
 import binascii
 import struct
 import encodings
@@ -14,77 +17,102 @@ import math
 import tf, tf2_ros
 
 
+
 class Interface(object):
 
-    def __init__(self):
+	def ms5837(self, data):
+		for i in range(self.window_size-1, -1, -1):
+			self.slider[i] = self.slider[i-1]
+		self.slider[0] = data.depth
+		self.depth = sum(self.slider)/self.window_size
+		#print self.depth
 
-        #self.pose_pub = rospy.Publisher("/static_point", PoseStamped, queue_size = 0)
-        rospy.Subscriber("imu/data", Imu, self.stim300)
-        rospy.Subscriber("imu/razor", Imu, self.razor)
+	def __init__(self):
 
-    def stim300(self, data):
+		#self.pose_pub = rospy.Publisher("/static_point", PoseStamped, queue_size = 0)
+		rospy.Subscriber("imu/data", Imu, self.stim300)
+		rospy.Subscriber("imu/razor", Imu, self.razor)
+		rospy.Subscriber("ms5837_pressure_sensor", ms5837, self.ms5837)
 
-	    br = tf2_ros.TransformBroadcaster()
-	    t = geometry_msgs.msg.TransformStamped()
+		self.rpy_pub = rospy.Publisher('rpy_msg', rpy_msg, queue_size=1)
 
-	    t.header.stamp = rospy.Time.now()
-	    t.header.frame_id = "world"
-	    t.child_frame_id = "stim300"
-	    t.transform.translation.x = 1.0
-	    t.transform.translation.y = 1.0
-	    t.transform.translation.z = 0.0
-	    #q = tf.transformations.quaternion_from_euler(0, 0, msg.theta)
-	    t.transform.rotation.x = data.orientation.x
-	    t.transform.rotation.y = data.orientation.y
-	    t.transform.rotation.z = data.orientation.z
-	    t.transform.rotation.w = data.orientation.w
+		self.depth = 0.0        
+		self.window_size = 5
+		self.slider = [0] * self.window_size
 
-	    br.sendTransform(t)
+	def stim300(self, data):
+		
+
+		br = tf2_ros.TransformBroadcaster()
+		t = geometry_msgs.msg.TransformStamped()
+
+		t.header.stamp = rospy.Time.now()
+		t.header.frame_id = "world"
+		t.child_frame_id = "stim300"
+		t.transform.translation.x = 1.0
+		t.transform.translation.y = 1.0
+		t.transform.translation.z = self.depth
+		t.transform.rotation.x = data.orientation.x
+		t.transform.rotation.y = data.orientation.y
+		t.transform.rotation.z = data.orientation.z
+		t.transform.rotation.w = data.orientation.w
+
+		br.sendTransform(t)
+
+		quaternion = (
+			data.orientation.x,
+			data.orientation.y,
+			data.orientation.z,
+			data.orientation.w)
+
+		roll_pitch_yaw = tf.transformations.euler_from_quaternion(quaternion)
+
+		rpy = rpy_msg()
+
+		rpy.header = Header(
+				stamp = rospy.get_rostime(),
+				frame_id = str("roll_pitch_yaw")
+			)
+
+		rpy.roll = roll_pitch_yaw[0]
+		rpy.pitch = roll_pitch_yaw[1]
+		rpy.yaw = roll_pitch_yaw[2]
+
+		self.rpy_pub.publish(rpy)
 
 
-        #br = tf2.TransformBroadcaster()
-        #br.sendTransform((0, 0, 0), (data.orientation.x,data.orientation.y,data.orientation.z,data.orientation.w), rospy.Time.now(), "stim_imu", "world")
-        #print data.orientation.x
+	def razor(self, data):
 
-    def razor(self, data):
+		br = tf2_ros.TransformBroadcaster()
+		t = geometry_msgs.msg.TransformStamped()
 
-	    br = tf2_ros.TransformBroadcaster()
-	    t = geometry_msgs.msg.TransformStamped()
+		t.header.stamp = rospy.Time.now()
+		t.header.frame_id = "world"
+		t.child_frame_id = "razor"
+		t.transform.translation.x = 1.0
+		t.transform.translation.y = 0.5
+		t.transform.translation.z = self.depth
+		
+		#q = tf.transformations.euler_from_quaternion(data.orientation)
+		#print data.orientation
+		#tf.transform(data.orientation, )
+		t.transform.rotation.x = data.orientation.x
+		t.transform.rotation.y = data.orientation.y
+		t.transform.rotation.z = (data.orientation.z)#math.sqrt(0.5)
+		t.transform.rotation.w = (data.orientation.w)#math.sqrt(0.5)
 
-	    t.header.stamp = rospy.Time.now()
-	    t.header.frame_id = "world"
-	    t.child_frame_id = "razor"
-	    t.transform.translation.x = 1.0
-	    t.transform.translation.y = 0.5
-	    t.transform.translation.z = 0.0
-	    
-	    #q = tf.transformations.euler_from_quaternion(data.orientation)
-	    #print data.orientation
-	    #tf.transform(data.orientation, )
-	    t.transform.rotation.x = data.orientation.x
-	    t.transform.rotation.y = data.orientation.y
-	    t.transform.rotation.z = (data.orientation.z)#math.sqrt(0.5)
-	    t.transform.rotation.w = (data.orientation.w)#math.sqrt(0.5)
-
-
-
-	    br.sendTransform(t)
-
-
-        #br = tf2.TransformBroadcaster()
-        #br.sendTransform((0, 0, 0), (data.orientation.x,data.orientation.y,data.orientation.z,data.orientation.w), rospy.Time.now(), "stim_imu", "world")
-        #print data.orientation.x        
+		br.sendTransform(t)
 
 def main():
 	rospy.init_node('orientation_node', anonymous=False)
 
 	Interface()
 
-        try:
+	try:
 		rospy.spin()
-        except rospy.ROSInterruptException:
+	except rospy.ROSInterruptException:
 		print "Shutting down"
-                pass
+		pass
 
 
 if __name__ == '__main__':
