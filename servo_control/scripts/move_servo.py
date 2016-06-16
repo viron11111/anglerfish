5266ee5e8a0f912af6685c93f368dc318b471031
andy <andygrayia@gmail.com>
2016-06-16 12:21:31 -0400

    ANGLERFISH: updated servo_control script

--- servo_control/scripts/move_servo.py
+++ servo_control/scripts/move_servo.py
@@ -6,39 +6,48 @@ import rospy
 
 from std_msgs.msg import Int16
 
-# Initialise the PWM device using the default address
-pwm = PWM(0x40)
-# Note if you'd like more debug output you can instead run:
-#pwm = PWM(0x40, debug=True)
+class servo_control():
 
-pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
+  def setServoPulse(channel, pulse):
+    pulseLength = 1000000                   # 1,000,000 us per second
+    pulseLength /= 60                       # 60 Hz
+    pulseLength /= 4096                     # 12 bits of resolution
+    pulse *= 1000
+    pulse /= pulseLength
+    self.pwm.setPWM(channel, 0, pulse)
 
-def setServoPulse(channel, pulse):
-  pulseLength = 1000000                   # 1,000,000 us per second
-  pulseLength /= 60                       # 60 Hz
-  print "%d us per period" % pulseLength
-  pulseLength /= 4096                     # 12 bits of resolution
-  print "%d us per bit" % pulseLength
-  pulse *= 1000
-  pulse /= pulseLength
-  pwm.setPWM(channel, 0, pulse)
-
-def moving(pos):
+  def moving(self, pos):
     if pos.data < 140:
-	rospy.logwarn("Servo min/max values exceeded: %d.  Acceptable range: 140-710" % pos.data)
-	pos.data = 140
+      rospy.logwarn("Servo min/max values exceeded: %d.  Acceptable range: 140-710" % pos.data)
+      pos.data = 140
     elif pos.data > 710:
-	rospy.logwarn("Servo min/max values exceeded: %d.  Acceptable range: 140-710" % pos.data)
-	pos.data = 710
+      rospy.logwarn("Servo min/max values exceeded: %d.  Acceptable range: 140-710" % pos.data)
+      pos.data = 710
 
-    pwm.setPWM(0, 0, pos.data)
-    print pos    
+    self.pwm.setPWM(0, 0, pos.data)    
 
-def servo_move():
-    rospy.init_node('move_servo', anonymous=True)
-    rospy.Subscriber("servo_position", Int16, moving)
+  def center():
+    pwm = PWM(0x40)
+    pwm.setPWMFreq(60)
+    pwm.setPWM(0, 0, 365)
 
+  def __init__(self):
+    rospy.Subscriber("servo_position", Int16, self.moving)
+    self.pwm = PWM(0x40)
+    self.pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
+
+  rospy.on_shutdown(center)
+
+def main(args):
+  rospy.init_node('move_servo', anonymous=False)
+
+  servo_control()
+
+  try:
     rospy.spin()
+  except rospy.ROSInterruptException:
+    print "Shutting down"
+    pass
 
 if __name__ == '__main__':
-	servo_move()
+  main(sys.argv)
