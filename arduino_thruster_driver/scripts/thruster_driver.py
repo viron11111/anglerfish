4d970c32a88184a2547b4f556d526a8e1c9f8afe
andy <andygrayia@gmail.com>
2016-08-17 15:47:16 -0400

    ANGLERFISH: work on thruster driver

--- arduino_thruster_driver/scripts/thruster_driver.py
+++ arduino_thruster_driver/scripts/thruster_driver.py
@@ -2,24 +2,57 @@
 
 import rospy
 import sys
-from std_msgs.msg import String
+#from arduino_thruster_driver.msg import ThrusterCmd
+from std_msgs.msg import Int16, String, Float32
 
-class ShooterController:
+class ThrusterDriver:
+    def name_func(self, name):
+
+        if name.data == 'TOP':
+            self.thrstr1.publish(self.thrust)
+        elif name.data == 'FL':
+            self.thrstr2.publish(self.thrust)
+        elif name.data == 'BL':
+            self.thrstr3.publish(self.thrust)
+        elif name.data == 'FR':
+            self.thrstr4.publish(self.thrust)
+        elif name.data == 'BR':
+            self.thrstr5.publish(self.thrust)
+        elif name.data == 'BTM':
+            self.thrstr6.publish(self.thrust)                                    
+
+    def thrust_func(self, force):
+        self.thrust = force.data * (32767)
+
     def __init__(self):
-        rospy.init_node("shooter_controller")
-        self.rate = rospy.Rate(10)
-    def control(self):
-        pub = rospy.Publisher("/shooter/control", String,queue_size=5)
+        rospy.Subscriber('name', String, self.name_func, queue_size = 1)
+        rospy.Subscriber('thrust', Float32, self.thrust_func, queue_size = 1)
+
+        self.thrstr1 = rospy.Publisher('thruster_cmd1', Int16, queue_size=1)
+        self.thrstr2 = rospy.Publisher('thruster_cmd2', Int16, queue_size=1)
+        self.thrstr3 = rospy.Publisher('thruster_cmd3', Int16, queue_size=1)
+        self.thrstr4 = rospy.Publisher('thruster_cmd4', Int16, queue_size=1)
+        self.thrstr5 = rospy.Publisher('thruster_cmd5', Int16, queue_size=1)
+        self.thrstr6 = rospy.Publisher('thruster_cmd6', Int16, queue_size=1)
+
+        rate = rospy.Rate(20)   
+        self.thrust = 0.0
+
         while not rospy.is_shutdown():
-            sys.stdout.write("Enter comand: ")
-            command = sys.stdin.readline().strip()
-            #rospy.loginfo(command)
-            pub.publish(command)
-            self.rate.sleep()
+            rate.sleep()
+
+def main(args):
+    rospy.init_node('t100_thruster_driver', anonymous=False)
+
+    ThrusterDriver()
+
+    try:
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        print "Shutting down"
+        pass
+    
+
 
 if __name__ == '__main__':
-    try:
-        sc = ShooterController()
-        sc.control();
-    except rospy.ROSInterruptException:
-        pass
+    main(sys.argv)
