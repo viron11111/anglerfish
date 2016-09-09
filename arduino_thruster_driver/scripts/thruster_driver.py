c6baf8cee679704471ef4a73cc2916932b06bcf3
andy <andygrayia@gmail.com>
2016-09-09 15:16:11 -0400

    ANGLERFISH: fixing thrusters

--- arduino_thruster_driver/scripts/thruster_driver.py
+++ arduino_thruster_driver/scripts/thruster_driver.py
@@ -21,25 +21,30 @@ class ThrusterDriver:
             self.command_thruster(thrust_cmd.name, thrust_cmd.thrust)
 
     def command_thruster(self, name, force):
+        if force > 1.0:
+            force = 1.0
+        elif force < -1.0:
+            force = -1.0
+
         if self.kill == False:
             if name == 'TOP':
                 self.thrust = force * (32767.0) #- 836
-                self.thrstr1.publish(self.thrust)
+                self.thrstr1.publish(int(self.thrust))
             elif name == 'FL':
                 self.thrust = force * (32767.0)
-                self.thrstr2.publish(self.thrust)
+                self.thrstr2.publish(int(self.thrust))
             elif name == 'BL':
                 self.thrust = force * (32767.0) #+ 404
-                self.thrstr3.publish(self.thrust)
+                self.thrstr3.publish(int(self.thrust))
             elif name == 'FR':
                 self.thrust = force * (32767.0) #+ 386
-                self.thrstr4.publish(self.thrust)
+                self.thrstr4.publish(int(self.thrust))
             elif name == 'BR':
                 self.thrust = force * (32767.0)
-                self.thrstr5.publish(self.thrust)
+                self.thrstr5.publish(int(self.thrust))
             elif name == 'BTM':
                 self.thrust = force * (32767.0)
-                self.thrstr6.publish(self.thrust)                                    
+                self.thrstr6.publish(int(self.thrust))                                    
         elif self.kill == True:
                 self.thrstr1.publish(0)
                 self.thrstr2.publish(0)
@@ -52,6 +57,21 @@ class ThrusterDriver:
 	    self.kill = kill_cmd.data
             #rospy.logwarn(self.kill)
             return SetBoolResponse(success = True, message = str(self.kill))
+
+    def stop_motor():
+        thrstr1 = rospy.Publisher('thruster_cmd1', Int16, queue_size=1)
+        thrstr2 = rospy.Publisher('thruster_cmd2', Int16, queue_size=1)
+        thrstr3 = rospy.Publisher('thruster_cmd3', Int16, queue_size=1)
+        thrstr4 = rospy.Publisher('thruster_cmd4', Int16, queue_size=1)
+        thrstr5 = rospy.Publisher('thruster_cmd5', Int16, queue_size=1)
+        thrstr6 = rospy.Publisher('thruster_cmd6', Int16, queue_size=1)
+        thrstr1.publish(0)
+        thrstr2.publish(0)
+        thrstr3.publish(0)
+        thrstr4.publish(0)
+        thrstr5.publish(0)
+        thrstr6.publish(0)
+
 
     def __init__(self):
         #rospy.Subscriber('name', String, self.name_func, queue_size = 1)
@@ -74,6 +94,8 @@ class ThrusterDriver:
 
         while not rospy.is_shutdown():
             rate.sleep()
+
+    rospy.on_shutdown(stop_motor)
 
 def main(args):
     rospy.init_node('t100_thruster_driver', anonymous=False)
