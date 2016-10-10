b264c69155b50e9d696d057f970bf944fad98497
viron11111 <andygrayia@gmail.com>
2016-10-10 19:57:22 -0400

    ANGLERFISH: joystick, added yaw

--- anglerfish_joy/nodes/joystick.py
+++ anglerfish_joy/nodes/joystick.py
@@ -34,21 +34,86 @@ buttons:
 
 class joystick(object):
 
-	def base_link(self, data):
-		self.subposx = data.pose.pose.position.x
-		self.subposy = data.pose.pose.position.y
-		self.subposz = data.pose.pose.position.z
-		self.subrotx = data.pose.pose.orientation.x
-		self.subroty = data.pose.pose.orientation.y
-		self.subrotz = data.pose.pose.orientation.z
-		self.subrotw = data.pose.pose.orientation.w
+	def base_link(self):#, data):
+		self.base_link_posx = 0#data.pose.pose.position.x
+		self.base_link_posy = 0#data.pose.pose.position.y
+		self.base_link_posz = 0#data.pose.pose.position.z
+		self.base_link_rotx = 0#data.pose.pose.orientation.x
+		self.base_link_roty = 0#data.pose.pose.orientation.y
+		self.base_link_rotz = 0#data.pose.pose.orientation.z
+		self.base_link_rotw = 1.0#data.pose.pose.orientation.w
+
+	def start(self):
+		self.subposx = 0
+		self.subposy = 0
+		self.subposz = 0
+		self.subrotx = 0
+		self.subroty = 0
+		self.subrotz = 0
+		self.subrotw = 1.0
 
 
 	def action(self, data):
-		br = tf2_ros.TransformBroadcaster()
-		t = geometry_msgs.msg.TransformStamped()
 
-		if data.buttons[0] == 1:
+
+		if data.buttons[1] == 1:
+			self.subposx = self.base_link_posx
+			self.subposy = self.base_link_posy
+			self.subposz = self.base_link_posz
+			self.subrotx = self.base_link_rotx
+			self.subroty = self.base_link_roty
+			self.subrotz = self.base_link_rotz
+			self.subrotw = self.base_link_rotw
+		elif data.buttons[2] == 1:
+			self.subposz += .1
+		elif data.buttons[0] == 1:
+			self.subposz -= .1
+
+		if data.axes[1] != 0.0:
+			self.subposx += data.axes[1]*.01
+
+		if data.axes[0] != 0.0:
+			self.subposy += data.axes[0]*.01
+
+		if data.axes[3] != 0.0:
+			quat = (
+				self.subrotx,
+				self.subroty,
+				self.subrotz,
+				self.subrotw)
+
+			euler = tf.transformations.euler_from_quaternion(quat)
+			
+			yaw = euler[2] + data.axes[3]*.1
+			euler = (
+				euler[0],
+				euler[1],
+				yaw)
+			#rospy.loginfo(euler)
+
+			quat = tf.transformations.quaternion_from_euler(euler[0],euler[1], euler[2])
+			self.subrotx = quat[0]
+			self.subroty = quat[1]
+			self.subrotz = quat[2]
+			self.subrotw = quat[3]
+
+		#odom = Odometry()
+
+
+	def __init__(self):
+
+		self.base_link()
+		rospy.Subscriber("joy", Joy, self.action)
+		#rospy.Subscriber("odometry/filtered", Odometry, self.base_link)
+
+		rate = rospy.Rate(20)
+		self.start()
+
+		while not rospy.is_shutdown():
+
+			br = tf2_ros.TransformBroadcaster()
+			t = geometry_msgs.msg.TransformStamped()
+
 			t.transform.translation.x = self.subposx
 			t.transform.translation.y = self.subposy
 			t.transform.translation.z = self.subposz
@@ -57,20 +122,29 @@ class joystick(object):
 			t.transform.rotation.z = self.subrotz
 			t.transform.rotation.w = self.subrotw
 
-		#odom = Odometry()
+			#odom = Odometry()
 
-		t.header.stamp = rospy.Time.now()
-		t.header.frame_id = "odom"
-		t.child_frame_id = "desired_position"
-		br.sendTransform(t)	
+			t.header.stamp = rospy.Time.now()
+			t.header.frame_id = "odom"
+			t.child_frame_id = "desired_position"
+			br.sendTransform(t)	
 
+			t.transform.translation.x = self.subposx
+			t.transform.translation.y = self.subposy
+			t.transform.translation.z = self.subposz
+			t.transform.rotation.x = self.subrotx
+			t.transform.rotation.y = self.subroty
+			t.transform.rotation.z = self.subrotz
+			t.transform.rotation.w = self.subrotw
 
+			#odom = Odometry()
 
-	def __init__(self):
+			t.header.stamp = rospy.Time.now()
+			t.header.frame_id = "map"
+			t.child_frame_id = "odom"
+			br.sendTransform(t)	
 
-		rospy.Subscriber("joy", Joy, self.action)
-		rospy.Subscriber("odometry/filtered", Odometry, self.base_link)
-
+			rate.sleep()
 
 def main():
 	rospy.init_node('joystick', anonymous=False)
