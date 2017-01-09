0099508c23ef9d885bcfecff935a0f774f9808cb
viron11111 <andygrayia@gmail.com>
2017-01-09 16:13:39 -0500

    INTEGRATED: added new data showing velocity and position displacement

--- integrate_accel/nodes/integrate.py
+++ integrate_accel/nodes/integrate.py
@@ -8,65 +8,36 @@ import numpy as np
 import tf, tf2_ros
 import math
 from sensor_msgs.msg import Imu
+import time
+
+import csv
+from time import localtime,strftime
+import datetime
 
 class integration():
 
+	def create_files(self):
+		self.start_time = 0
+		self.time_diff = 0
+
+		date_time = strftime("%d_%m_%y_%H_%M_%S", localtime())
+		file_name1 = "/home/andy/catkin_ws/src/anglerfish/integrate_accel/data/razor_%s.csv" % date_time
+		
+		self.velocity_file = csv.writer(open(file_name1,'w'))
+		self.velocity_file.writerow(["Time", "Velocity", "Position"])
+
 	def linear_accel(self, data):
+		self.new_time = rospy.get_rostime()
 
 		self.x_accel = data.linear_acceleration.x
 		self.y_accel = data.linear_acceleration.y
 		self.z_accel = data.linear_acceleration.z
 
-		if self.init_vars == 0:
-			holder = 0.0
-			x_vel_old = 0.0
-			x_vel = 0.0
-			y_vel = 0.0
-			z_vel = 0.0
-			x_accel_diff_old = 0.0
-			x_pos = 0.0
-			y_pos = 0.0
-			z_pos = 0.0
-			fir1 = 0
-			fir2 = 0
-			fir3 = 0
-			fir4 = 0.0
-			measfir1 = 0.0
-			self.measfir2 = 0.0
-			self.measfir3 = 0.0
-			self.measfir4 = 0.0
-			self.measfir5 = 0.0
-			orinfir1 = 0.0
-			self.orinfir2 = 0.0
-			orinfir3 = 0.0
-			orinfir4 = 0.0
-			orinfir5 = 0.0
-
-			self.accn1 = 0.0
-			self.accn2 = 0.0
-			self.accy0 = data.linear_acceleration.x
-
-			velfir1 = 0.0
-			velfir2 = 0.0
-			accfir1 = 0.0
-			accfir2 = 0.0
-			vel = 0.0
-			posfir1 = 0.0
-			posfir2 = 0.0
-			pos = 0.0
-			diff1 = 0.0
-			diff2 = 0.0
-			self.position = 0.0
-			self.orientation = 0.0 #self.x_accel
-			self.vx = self.x_accel
-
-			self.init_vars = 1
-
-
-
-		self.new_time = rospy.get_rostime()
 		self.time_diff = self.new_time - self.old_time
 		self.sum_time = self.time_diff.secs + self.time_diff.nsecs/1000000000.0
+
+		time = self.new_time - self.start_time
+		run_time = time.secs + time.nsecs/1000000000.0
 
 		quat = (
 			data.orientation.x,
@@ -85,49 +56,73 @@ class integration():
 		self.subrotz = 0.0 # quat[2]
 		self.subrotw = 0.0 # quat[3]
 		
-		self.y_grav = -math.cos(self.pitch)*math.sin(self.roll)*-9.80665
+		self.y_grav = math.cos(self.pitch)*math.sin(self.roll)*-9.80665
 		self.x_grav = math.sin(self.pitch)*-9.80665
 		self.z_grav = -math.cos(self.pitch)*math.cos(self.roll)*-9.80665
+
+		#rospy.loginfo(self.x_grav)
+		#ime.sleep(1)
 
 		#linear acceleration
 		#FIR low pass filter
 		measfir1 = self.x_accel
-		measured = measfir1*0.2 + self.measfir2*0.2 +self.measfir3*0.2 + self.measfir4*0.2 + self.measfir5*0.2
-		self.measfir5 = self.measfir4
-		self.measfir4 = self.measfir3
-		self.measfir3 = self.measfir2
+		self.measured = measfir1*0.5 + self.measfir2*0.5 # +self.measfir3*0.25 + self.measfir4*0.25# + self.measfir5*0.2
+		#self.measfir5 = self.measfir4
+		#self.measfir4 = self.measfir3
+		#self.measfir3 = self.measfir2
 		self.measfir2 = measfir1
 
 		#FIR high pass filter
-		'''measfir1 = self.x_accel
-		measured = 2*(measfir1*0.5 - self.measfir2*0.5)
-		self.measfir2 = measfir1'''
+		#measfir1 = self.x_accel
+		#self.measured = self.measfir2 - 2*(measfir1*0.5 - self.measfir2*0.5)
+		#self.measfir2 = measfir1
 
 		#Estimate gravity experience
 		#FIR low pass filter
-		orinfir1 = self.x_grav
+		'''orinfir1 = self.x_grav
 		orientation = orinfir1*0.5 + self.orinfir2*0.5
-		self.orinfir2 = orinfir1	
+		self.orinfir2 = orinfir1'''	
 
 		#Estimate gravity experience
 		#FIR high pass filter
-		'''orinfir1 = self.x_grav
-		self.orientation = self.orientation + (orinfir1*0.5 - self.orinfir2*0.5)
-		self.orinfir2 = orinfir1'''
+		orinfir1 = self.x_grav
+		self.orientation = self.orinfir2 - 2*(orinfir1*0.5 - self.orinfir2*0.5)
+		self.orinfir2 = orinfir1
 
-		acceleration = measured- orientation #(measured - orientation) #x_pos + vel
+
+
+		acceleration = self.x_accel - self.x_grav #self.orientation #(measured - orientation) #x_pos + vel
 
 		velocity = acceleration*self.sum_time#acceleration*self.sum_time
 
-		self.speed = self.speed + velocity
+		self.velfir1 = velocity
+
+		'''if self.holder3 == 0.0:
+			rospy.loginfo("zero")
+			self.velfiri1 = 0.0
+			time.sleep(1)'''
+
+
+		self.speed = self.speed + velocity #self.velfir2 - 2*(self.velfir1*0.5 - self.velfir2*0.5)
+
+
+		#self.velfir2 = self.velfir1
+
+		#rospy.loginfo(self.speed)
+		#time.sleep(1)
+
+		self.vel_new = velocity
+		velocity_print = (self.vel_old - self.vel_new)/self.sum_time
+		#rospy.loginfo(velocity)
+		self.vel_old = self.vel_new
 
 		self.position = self.position + self.speed*self.sum_time
 
 		self.pospub.header.stamp = rospy.Time.now()
 		self.pospub.header.frame_id = 'desired_position' # i.e. '/odom'
 
-		self.pospub.pose.pose.position.x = orientation #estimated_gravity
-		self.pospub.pose.pose.position.y = measured #linear Acceleration
+		self.pospub.pose.pose.position.x = self.speed #estimated_gravity
+		self.pospub.pose.pose.position.y = self.position #linear Acceleration
 		self.pospub.pose.pose.position.z = velocity
 		self.pospub.pose.pose.orientation.x = self.subrotx
 		self.pospub.pose.pose.orientation.y = self.subroty
@@ -138,34 +133,127 @@ class integration():
 		self.pose_pub.publish(self.pospub)
 		self.old_time = rospy.get_rostime()
 
+		self.velocity_file.writerow([run_time, self.speed, self.position])
+
+
+		#rospy.loginfo(run_time)
+
+
 	def orientation(self, data):
-		if self.calculated_flag == 0:
-			self.qW = np.array([data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w]) 
-			self.w = np.array([data.twist.twist.angular.x, data.twist.twist.angular.y, data.twist.twist.angular.z])
 
-			#this section converts quaternions to euler, applys yaw, then converts euler back to quaternion
-			'''quat = (
-				self.qW[0],
-				self.qW[1],
-				self.qW[2],
-				self.qW[3])
+		self.qW = np.array([data.pose.pose.orientation.x, data.pose.pose.orientation.y, data.pose.pose.orientation.z, data.pose.pose.orientation.w]) 
+		self.w = np.array([data.twist.twist.angular.x, data.twist.twist.angular.y, data.twist.twist.angular.z])
 
-			euler = tf.transformations.euler_from_quaternion(quat)
+		self.new_time = rospy.get_rostime()
 
-			roll = euler[0]
-			pitch = euler[1]
-			yaw = euler[2]
+		self.x_accel = data.twist.twist.linear.x
+		self.y_accel = data.twist.twist.linear.y
+		self.z_accel = data.twist.twist.linear.z
 
-			quaternion = tf.transformations.quaternion_from_euler(roll, pitch, yaw)
-			self.subrotx = quat[0]
-			self.subroty = quat[1]
-			self.subrotz = quat[2]
-			self.subrotw = quat[3]
-			
-			self.y_grav = -math.cos(pitch)*math.sin(roll)*-9.80665
-			self.x_grav = math.sin(pitch)*-9.80665
-			self.z_grav = -math.cos(pitch)*math.cos(roll)*-9.80665
-			self.calculated_flag = 1'''
+		self.time_diff = self.new_time - self.old_time
+		self.sum_time = self.time_diff.secs + self.time_diff.nsecs/1000000000.0
+
+		time = self.new_time - self.start_time
+		run_time = time.secs + time.nsecs/1000000000.0
+
+		quat = (
+			data.pose.pose.orientation.x,
+			data.pose.pose.orientation.y,
+			data.pose.pose.orientation.z,
+			data.pose.pose.orientation.w)
+
+		euler = tf.transformations.euler_from_quaternion(quat)
+
+		self.roll = euler[0]
+		self.pitch = euler[1]
+		self.yaw = euler[2]
+
+		self.subrotx = 0.0 # quat[0]
+		self.subroty = 0.0 # quat[1]
+		self.subrotz = 0.0 # quat[2]
+		self.subrotw = 0.0 # quat[3]
+		
+		self.y_grav = math.cos(self.pitch)*math.sin(self.roll)*-9.80665
+		self.x_grav = math.sin(self.pitch)*-9.80665
+		self.z_grav = -math.cos(self.pitch)*math.cos(self.roll)*-9.80665
+
+		#rospy.loginfo(self.x_grav)
+		#ime.sleep(1)
+
+		#linear acceleration
+		#FIR low pass filter
+		measfir1 = self.x_accel
+		self.measured = measfir1*0.5 + self.measfir2*0.5 # +self.measfir3*0.25 + self.measfir4*0.25# + self.measfir5*0.2
+		#self.measfir5 = self.measfir4
+		#self.measfir4 = self.measfir3
+		#self.measfir3 = self.measfir2
+		self.measfir2 = measfir1
+
+		#FIR high pass filter
+		#measfir1 = self.x_accel
+		#self.measured = self.measfir2 - 2*(measfir1*0.5 - self.measfir2*0.5)
+		#self.measfir2 = measfir1
+
+		#Estimate gravity experience
+		#FIR low pass filter
+		'''orinfir1 = self.x_grav
+		orientation = orinfir1*0.5 + self.orinfir2*0.5
+		self.orinfir2 = orinfir1'''	
+
+		#Estimate gravity experience
+		#FIR high pass filter
+		orinfir1 = self.x_grav
+		self.orientation = self.orinfir2 - 2*(orinfir1*0.5 - self.orinfir2*0.5)
+		self.orinfir2 = orinfir1
+
+
+
+		acceleration = self.x_accel - self.x_grav #self.orientation #(measured - orientation) #x_pos + vel
+
+		velocity = acceleration*self.sum_time#acceleration*self.sum_time
+
+		self.velfir1 = velocity
+
+		'''if self.holder3 == 0.0:
+			rospy.loginfo("zero")
+			self.velfiri1 = 0.0
+			time.sleep(1)'''
+
+
+		self.speed = self.speed + velocity #self.velfir2 - 2*(self.velfir1*0.5 - self.velfir2*0.5)
+
+
+		#self.velfir2 = self.velfir1
+
+		#rospy.loginfo(self.speed)
+		#time.sleep(1)
+
+		self.vel_new = velocity
+		velocity_print = (self.vel_old - self.vel_new)/self.sum_time
+		#rospy.loginfo(velocity)
+		self.vel_old = self.vel_new
+
+		self.position = self.position + self.speed*self.sum_time
+
+		self.pospub.header.stamp = rospy.Time.now()
+		self.pospub.header.frame_id = 'desired_position' # i.e. '/odom'
+
+		self.pospub.pose.pose.position.x = self.speed #estimated_gravity
+		self.pospub.pose.pose.position.y = self.position #linear Acceleration
+		self.pospub.pose.pose.position.z = velocity
+		self.pospub.pose.pose.orientation.x = self.subrotx
+		self.pospub.pose.pose.orientation.y = self.subroty
+		self.pospub.pose.pose.orientation.z = self.subrotz
+		self.pospub.pose.pose.orientation.w = self.subrotw
+		#self.pospub.pose.covariance=(np.eye(6)*.001).flatten()
+
+		self.pose_pub.publish(self.pospub)
+		self.old_time = rospy.get_rostime()
+
+		self.velocity_file.writerow([run_time, self.speed, self.position])
+
+
+		#rospy.loginfo(run_time)		
 
 
 	def __init__(self):
@@ -173,7 +261,9 @@ class integration():
 		self.init_vars = 0
 		self.init_vars2 = 0
 
-		rospy.Subscriber("/odometry/filtered", Odometry, self.orientation)
+		self.create_files()
+
+		#rospy.Subscriber("/odometry/filtered", Odometry, self.orientation)
 		rospy.Subscriber("/imu/data", Imu, self.linear_accel)
 
 		self.pose_pub = rospy.Publisher("integrated_position", PoseWithCovarianceStamped, queue_size = 1)
@@ -193,6 +283,8 @@ class integration():
 		self.z_grav = 0.0
 		self.time_diff = 0.0
 		self.sum_time = 0.0
+		self.start_time = rospy.get_rostime()
+
 		self.accel_flag = 0
 		self.calculated_flag = 0
 
@@ -203,11 +295,38 @@ class integration():
 
 		self.roll = 0.0
 		self.pitch = 0.0
+		self.pitch2 = 0.0
 		self.yaw = 0.0
 
 		self.speed = 0.0
+		self.measfir2 = 0.0
+		self.measfir3 = 0.0
+		self.measfir4 = 0.0
+		self.measfir5 = 0.0
+		self.orinfir2 = 0.0
 
-		r = rospy.Rate(20)
+		self.velfir1 = 0.0
+		self.velfir2 = 0.0
+
+		self.holder1 = 0.0
+		self.holder2 = 0.0
+		self.holder3 = 0.0
+		self.holder4 = 0.0
+		self.holder5 = 0.0
+		self.holder6 = 0.0
+		self.holder7 = 0.0
+		self.holder8 = 0.0
+		self.holder9 = 0.0
+
+		self.pos_old = 0.0
+		self.pos_new = 0.0
+
+		self.vel_old = 0.0
+		self.vel_new = 0.0
+
+		self.position = 0.0
+
+		r = rospy.Rate(60)
 
 		while not rospy.is_shutdown():
 			r.sleep()
