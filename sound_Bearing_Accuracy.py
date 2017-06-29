3ad8621b26e78e0cd3f2ed1a568e38f6c97b6c4d
andy <andygrayia@gmail.com>
2017-06-29 14:15:23 -0400

    SOUND: working on sound simulator

--- sound_Bearing_Accuracy.py
+++ sound_Bearing_Accuracy.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 
-sampling_frequency = 100000
-signal_frequency = 43000
-distance_between_hydrophones = 0.05 #meters
-speed_of_sound = 1484 #meters per second
+sampling_frequency = 100000.0
+signal_frequency = 43000.0
+distance_between_hydrophones = 1 #meters
+speed_of_sound = 1484.0 #meters per second
 
 #0.5 assuming signal can only be detected on peaks and troughs
 wave_detection_threshold = 0.5
 
+time_between_samples = 1 / sampling_frequency
+signal_time_period = 1 / signal_frequency
 time_signal_below_threshold = wave_detection_threshold*(1/signal_frequency)
 
-print time_signal_below_threshold
+travel_time = distance_between_hydrophones / speed_of_sound
+
+x = np.linspace(-9, 9, 400)
+y = np.linspace(-5, 5, 400)
+x, y = np.meshgrid(x, y)
+
+def axes():
+    plt.axhline(0, alpha=.1)
+    plt.axvline(0, alpha=.1)
+
+a = .3
+axes()
+plt.contour(x, y, (y**2 + 4*a*x), [0], colors='k')
+plt.show()
