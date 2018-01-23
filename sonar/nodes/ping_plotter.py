63ee03e84174fd0932418d66ab1e49dfd01adff9
viron11111 <andygrayia@gmail.com>
2018-01-23 09:07:46 -0500

    SONAR_PLOTTER: combined ping raw and ping conditioned into one display

--- sonar/nodes/ping_plotter.py
+++ sonar/nodes/ping_plotter.py
@@ -23,8 +23,8 @@ import scipy.fftpack
 import operator
 
 class plotter():
-
-    def plot_ping(self,data):       
+    
+    def ping_raw(self,data):       
 
         self.t = []
         self.a = []
@@ -100,6 +100,82 @@ class plotter():
         if len(self.t) > n:
             self.t = self.t[:-1]  #make sure len(t) is = to len(n), shave the last number off            
 
+    def plot_ping(self,data):       
+
+        self.t2 = []
+        self.a2 = []
+        self.b2 = []
+        self.c2 = []
+        self.d2 = []
+
+        channels = data.channels
+        #samples = float(data.samples/channels)
+        #print samples
+        sample_rate = data.sample_rate
+        #print sample_rate
+        adc_bit = data.adc_bit
+        values = data.data
+
+        #values = values[15000:21000:1]
+        
+        samples = float(data.samples) #float(len(data)/channels)
+
+        time = (samples/float(sample_rate))#*10**6
+
+        #print("data received from service")
+
+        length = samples*channels
+        self.x_axis_length2 = (samples/4.0)*(1.0/sample_rate)
+
+        #print samples
+        #print self.x_axis_length
+
+        starting_sample = 0
+        distance = (float(length)/float(samples))*time
+
+        #self.x = np.arange(starting_sample*(time/samples),distance, time/samples)
+        #print self.x
+        self.a2 = values[starting_sample + 0:int(length):channels]
+        #print self.a
+        self.b2 = values[starting_sample + 1:int(length):channels]
+        self.c2 = values[starting_sample + 2:int(length):channels]
+        self.d2 = values[starting_sample + 3:int(length):channels]
+
+        #print len(self.a1)
+        #print range(len(self.a1)-1)
+
+        self.a3 = [0]*int(samples/4)
+        self.b3 = [0]*int(samples/4)
+        self.c3 = [0]*int(samples/4)
+        self.d3 = [0]*int(samples/4)
+        #print samples/4
+
+        '''for i in range(len(self.a)-3):
+            #print i
+            self.a1[i] = self.a[i]*(0.25) + self.a[i+1]*(0.25) + self.a[i+2]*(0.25) + self.a[i+3]*(0.25)
+            self.b1[i] = self.b[i]*(0.25) + self.b[i+1]*(0.25) + self.b[i+2]*(0.25) + self.b[i+3]*(0.25)
+            self.c1[i] = self.c[i]*(0.25) + self.c[i+1]*(0.25) + self.c[i+2]*(0.25) + self.c[i+3]*(0.25)
+            self.d1[i] = self.d[i]*(0.25) + self.d[i+1]*(0.25) + self.d[i+2]*(0.25) + self.d[i+3]*(0.25)'''
+
+        self.N2 = len(self.a2)   #number of samples in channel1
+        Fs = sample_rate
+        Ts = 1.0/Fs    
+        #x = np.linspace(0.0, N*Ts, N)    
+        self.yf2 = scipy.fftpack.fft(self.a2)
+        index, value = max(enumerate(abs(self.yf2)), key=operator.itemgetter(1))
+
+        self.xf2 = np.linspace(0.0, 1.0/(2.0*Ts), self.N2/2)
+
+        legends = [None]*channels  #set up ledgends for x channels
+        wave = [None]*len(self.a2)  #make empty list
+
+        n = len(self.a2)   #length of samples (samplecount/4 from AdvanTech driver c++)
+
+        self.t2 = np.arange(0,n*Ts,Ts)  #resolution of sampling, ie 1 MS/s = 1*10^-6
+
+        if len(self.t2) > n:
+            self.t2 = self.t2[:-1]  #make sure len(t) is = to len(n), shave the last number off            
+
 
     def __init__(self):
         rospy.init_node('ping_plotter')
@@ -109,19 +185,28 @@ class plotter():
 
         #self.x = []
         self.a = []
+        self.a2 = []
         self.a1 = []
         self.b = []
+        self.b2 = []
         self.c = []
+        self.c2 = []
         self.d = []
+        self.d2 = []
         self.trigger = False
         self.t  = []
+        self.t2  = []
         self.x_axis_length = 0
+        self.x_axis_length2 = 0
         self.xf = 0
         self.N = 1
         self.yf = [0]
+        self.xf2 = 0
+        self.N2 = 1
+        self.yf2 = [0]        
 
         plt.ion()
-        fig, self.ax = plt.subplots(2, 1)         
+        fig, self.ax = plt.subplots(3, 1)         
         #fig, self.ax = plt.subplots(1, 1)  
 
         while not rospy.is_shutdown():
@@ -129,7 +214,7 @@ class plotter():
             #ping = ping_service()
 
             #rospy.Subscriber('/hydrophones/pingmsg', Pingdata, self.plot_ping) #for simulation
-            #rospy.Subscriber('/hydrophones/pingraw', Pingdata, self.plot_ping)
+            rospy.Subscriber('/hydrophones/pingraw', Pingdata, self.ping_raw)
             rospy.Subscriber('/hydrophones/pingconditioned', Pingdata, self.plot_ping)
 
 
@@ -149,6 +234,21 @@ class plotter():
                 xfval = self.xf
                 yfval = self.yf
 
+                xvalues2 = []
+                avalues2 = []
+                bvalues2 = []
+                cvalues2 = []
+                dvalues2 = []
+
+                xvalues2 = self.t2
+                avalues2 = self.a2
+                bvalues2 = self.b2
+                cvalues2 = self.c2
+                dvalues2 = self.d2
+                Nval2 = self.N2
+                xfval2 = self.xf2
+                yfval2 = self.yf2                
+
                 self.ax[0].cla()
                 #print "%i, %i" % (len(self.t), len(self.a))
                 self.ax[0].plot(xvalues,avalues, linewidth=2.0, label='Hydrophone A')
@@ -157,28 +257,46 @@ class plotter():
                 self.ax[0].plot(xvalues,dvalues, linewidth=2.0, label='Hydrophone D')
 
                 self.ax[0].legend(loc="upper left", fontsize=10)
-                self.ax[0].set_title("Actual Received Signals", weight = 'bold', size = 37, x = 0.5, y = 1.02, horizontalalignment='center')
-                self.ax[0].set_xlabel('Time (seconds)', size = 25, weight = 'bold', x = 0.5, y = 0)
-                self.ax[0].set_ylabel('Amplitude', size = 25, weight = 'bold', x = 0, y = 0.5)
+                self.ax[0].set_title("Raw Signals")#, weight = 'bold', size = 37, x = 0.5, y = 1.02, horizontalalignment='center')
+                #self.ax[0].set_xlabel('Time (seconds)', size = 25, weight = 'bold', x = 0.5, y = 0)
+                self.ax[0].set_ylabel('Amplitude')#, size = 25, weight = 'bold', x = 0, y = 0.5)
                 #self.ax[0].set_ylim(-5,5)
                 self.ax[0].set_xlim(0,self.x_axis_length)
-                self.ax[0].tick_params(axis='both', which='major', labelsize=25, pad=20)
-                self.ax[0].tick_params(axis='both', which='minor', labelsize=25, pad=20)
+                #self.ax[0].tick_params(axis='both', which='major', labelsize=25, pad=20)
+                #self.ax[0].tick_params(axis='both', which='minor', labelsize=25, pad=20)
                 self.ax[0].xaxis.labelpad = 20
                 self.ax[0].yaxis.labelpad = 20
 
-
                 self.ax[1].cla()
-                #self.ax[2].set_title("FFT On Channel One")
+                #print "%i, %i" % (len(self.t), len(self.a))
+                self.ax[1].plot(xvalues2,avalues2, linewidth=2.0, label='Hydrophone A')
+                self.ax[1].plot(xvalues2,bvalues2, linewidth=2.0, label='Hydrophone B')
+                self.ax[1].plot(xvalues2,cvalues2, linewidth=2.0, label='Hydrophone C')
+                self.ax[1].plot(xvalues2,dvalues2, linewidth=2.0, label='Hydrophone D')
+
+                self.ax[1].legend(loc="upper left", fontsize=10)
+                self.ax[1].set_title("Conditioned Signals")#, weight = 'bold', size = 12, x = 0.5, y = 1.02, horizontalalignment='center')
+                #self.ax[1].set_xlabel('Time (seconds)', size = 25, weight = 'bold', x = 0.5, y = 0)
+                self.ax[1].set_ylabel('Amplitude')#, size = 25, weight = 'bold', x = 0, y = 0.5)
+                #self.ax[0].set_ylim(-5,5)
+                self.ax[1].set_xlim(0,self.x_axis_length2)
+                #self.ax[1].tick_params(axis='both', which='major', labelsize=25, pad=20)
+                #self.ax[1].tick_params(axis='both', which='minor', labelsize=25, pad=20)
+                self.ax[1].xaxis.labelpad = 20
+                self.ax[1].yaxis.labelpad = 20                
+
+
+                self.ax[2].cla()
+                self.ax[2].set_title("FFT On Channel One")
                 #self.ax[1].plot(frq,abs(Y),'r') # plotting the FFT spectrum
                 if self.yf[0] != 0:
-                    self.ax[1].plot(xfval,2.0/Nval * np.abs(yfval[:Nval//2]),'r') # plotting the FFT spectrum
+                    self.ax[2].plot(xfval,2.0/Nval * np.abs(yfval[:Nval//2]),'r') # plotting the FFT spectrum
                 #print abs(Y)
-                self.ax[1].set_xlim(5000,50000)
+                self.ax[2].set_xlim(5000,50000)
                 #plt.xticks(np.arange(5000, 50000+1, 500.0))
                 #self.ax[1].set_ylim(0,n/10)
-                self.ax[1].set_xlabel('Freq (Hz)')
-                self.ax[1].set_ylabel('|Y(freq)|')
+                self.ax[2].set_xlabel('Freq (Hz)')
+                self.ax[2].set_ylabel('|Y(freq)|')
 
                 plt.pause(0.05)
 
